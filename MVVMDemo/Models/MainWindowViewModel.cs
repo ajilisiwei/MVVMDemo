d1b801964bfc1fe3b34e80b61eb6ec47377a649f
daichiwei <daichiwei@szteyou.com>
2016-07-12 09:44:56 +0800

    A Simple Demo About MVVM Pattern

---
+++ MVVMDemo/Models/MainWindowViewModel.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace MVVMDemo
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                var e = new PropertyChangedEventArgs(propertyName);
+                PropertyChanged(this, e);
+            }
+        }
+
+
+
+        private string _name;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                this._name = value;
+                this.OnPropertyChanged("Name");
+            }
+        }
+
+
+        private string _greetWords;
+
+        public string GreetWords
+        {
+            get
+            {
+                return _greetWords;
+            }
+            set
+            {
+                this._greetWords = value;
+                this.OnPropertyChanged("GreetWords");
+            }
+        }
+        
+
+
+        #region SayHelloCmd
+
+        private RelayCommand _sayHelloCmd;
+
+        public RelayCommand SayHelloCmd { get { return _sayHelloCmd ?? (_sayHelloCmd = new RelayCommand(SayHello)); } }
+
+        private void SayHello(object para)
+        {
+            GreetWords = "Hello! " + Name;
+        }
+        #endregion
+
+
+    }
+}
---
+++ MVVMDemo/Models/RelayCommand.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace MVVMDemo
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object> _execute;
+        private readonly Action _exeNoPara;
+
+        private readonly Predicate<object> _canExecute;
+
+        public RelayCommand(Action<object> execute)
+        {
+            _execute = execute;
+        }
+
+        public RelayCommand(Action execute)
+        {
+            this._exeNoPara = execute;
+        }
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            if (execute == null)
+            {
+                throw new ArgumentNullException("execute");
+            }
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;//默认都可以执行
+            if (this._canExecute != null)
+                return this._canExecute(parameter);
+            return true;
+        }
+        //命令能不能执行的状态发生改变时，通知命令的调用者
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+        //当命令执行是执行的方法
+        public void Execute(object parameter)
+        {
+            if (this._execute != null)
+                this._execute(parameter);
+            if (this._exeNoPara != null)
+                this._exeNoPara();
+        }
+    }
+}
